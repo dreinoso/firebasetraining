07429695f083874d8fc0a0f0d7829b5c5f3d74b3
Denis Reinoso <reinoso.denis@gmail.com>
2019-07-15 22:21:39 -0300

    Add initial project structure
    Signed-off-by: Denis Reinoso <reinoso.denis@gmail.com>

---
+++ app/src/main/java/com/chainreaction/firebasetraining/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.chainreaction.firebasetraining.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chainreaction.firebasetraining.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
